8a1e3b48a626d66aaf33e6b1efde6cfe3b723fb8
mishamlan <mishkat.amlan@stonybrook.edu>
2024-10-09 11:47:25 -0400

    server

---
+++ server/src/main/java/com/example/cse416/Cse416Application.java
@@ -0,0 +1,20 @@
+package com.example.cse416;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Cse416Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cse416Application.class, args);
+	}
+
+	@GetMapping("/")
+	public String root(){
+		return "Hello, World!";
+	}
+}
