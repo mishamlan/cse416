1e42e3bb7f5a1b55a916ecbccb3d72ffb30230d7
mishamlan <mishkat.amlan@stonybrook.edu>
2024-11-21 21:31:33 -0500

    1

--- server/src/main/java/com/example/cse416/model/Ensemble.java
+++ server/src/main/java/com/example/cse416/model/Ensemble.java
@@ -3,7 +3,12 @@ package com.example.cse416.model;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.springframework.data.annotation.Id;
+
 import com.example.cse416.constants.Group;
+import com.example.cse416.constants.Type;
+import com.example.cse416.constants.StateID;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +19,11 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Ensemble {
-    ArrayList<DistrictPlan> plans;
-    ArrayList<Candidate> winners;
-    HashMap<Group, Integer> demographics;
+    @Id
+    private String id;
+    private Type type;
+    private StateID name;
+    private ArrayList<DistrictPlan> plans;
+    private ArrayList<Candidate> winners;
+    private HashMap<Group, Integer> demographics;
 }
